6130decc0fa542ff7da02ce375d558e84e1bcf4b
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-15 14:22:22 +0530

    setup route & controller file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 
+const cors = require("cors");
+const eventRoutes = require("./routes/event.routes");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+// routes
+
+app.use("/event", eventRoutes);
+
 app.get("/", (req, res) => {
-  res.send("event api running .........");
+  res.send("event api running ........");
 });
 
 app.listen(PORT, () => {
